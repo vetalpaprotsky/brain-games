bf031cb785bccfa210de0accfd0d02f12bf5d8e3
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-07-11 14:38:11 +0300

    use 'operator' module in calculator game to make code a bit simpler

--- brain_games/games/calculator.py
+++ brain_games/games/calculator.py
@@ -1,4 +1,5 @@
 import random
+import operator
 
 
 DESCRIPTION = 'What is the result of the expression?'
@@ -7,17 +8,15 @@ DESCRIPTION = 'What is the result of the expression?'
 def get_question_and_correct_answer():
     LOWEST_RAND_NUM = 1
     BIGGEST_RAND_NUM = 25
-    OPERATIONS = ['+', '-', '*']
+    OPERATIONS = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul
+    }
     first_operand = random.randint(LOWEST_RAND_NUM, BIGGEST_RAND_NUM)
     second_operand = random.randint(LOWEST_RAND_NUM, BIGGEST_RAND_NUM)
-    operation = random.choice(OPERATIONS)
+    operation = random.choice(list(OPERATIONS))
     question = '{} {} {}'.format(first_operand, operation, second_operand)
-
-    if operation == '+':
-        correct_answer = first_operand + second_operand
-    elif operation == '-':
-        correct_answer = first_operand - second_operand
-    else:
-        correct_answer = first_operand * second_operand
+    correct_answer = OPERATIONS[operation](first_operand, second_operand)
 
     return question, correct_answer
