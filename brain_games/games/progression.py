3dc5fc3c13e84762e67f3eebab9ccb80f80d5216
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-07-11 14:52:04 +0300

    make generate_progression function more flexible

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,18 +4,18 @@ import random
 DESCRIPTION = 'What number is missing in the progression?'
 
 
-def generate_progression(length):
-    PROG_STEP = random.randint(1, 10)
-    PROG_FIRST_NUM = random.randint(1, 100)
-    PROG_LAST_NUM = PROG_FIRST_NUM + length * PROG_STEP
-    progression = list(range(PROG_FIRST_NUM, PROG_LAST_NUM, PROG_STEP))
+def generate_progression(first_number, step, length):
+    last_number = first_number + length * step
+    progression = list(range(first_number, last_number, step))
 
     return progression
 
 
 def get_question_and_correct_answer():
     PROG_LENGTH = 10
-    progression = generate_progression(PROG_LENGTH)
+    PROG_STEP = random.randint(1, 10)
+    PROG_FIRST_NUM = random.randint(1, 100)
+    progression = generate_progression(PROG_FIRST_NUM, PROG_STEP, PROG_LENGTH)
     random_element_index = random.randint(0, PROG_LENGTH - 1)
     correct_answer = progression[random_element_index]
     progression[random_element_index] = '..'
